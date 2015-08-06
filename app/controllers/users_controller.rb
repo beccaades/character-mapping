679e2d6e26a571b9bae873d7a77d93564fc4c6ae
beccaades <rebecca.ades@gmail.com>
2015-08-06 18:29:15 -0400

    sessions ability restored

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,17 +8,10 @@ class UsersController < ApplicationController
   def create
     @user=User.new(user_params)
 
-    respond_to do |format|
-      if @user.save
-        #Tell the UserMailer to send a welcome email after save
-        UserMailer.welcome_email(@user).deliver_now
-
-        format.html {redirect_to(@user, notice: 'Now please login')}
-        format.json {render json: @user, status: :created, location: @user }
-      else
-        format.html {render action: 'new'}
-        format.json {render json: @user.errors, status: :unprocessable_entity}
-      end
+    if @user.save
+      redirect_to login_path, :notice => "Now please login"
+    else
+      render :new
     end
   end
 
--- app/mailers/user_mailer.rb
+++
@@ -1,9 +0,0 @@
-class UserMailer < ApplicationMailer
-  default from: 'rebecca.ades@gmail.com'
-
-  def welcome_email(user)
-    @user = user
-    email_with_name = %("#{@user.name}" <#{@user.email}>)
-    mail to: @user.email, subject: 'Welcome to Character Mapping!'
-  end
-end
